52f4ecfd3a5f3f94d0d0582d41def5a981935e1f
unknown <tasrifnaazmul@gmai.com>
2022-11-14 16:10:01 +0600

    update 2

--- Final/Lab task 6/display.php
+++ Final/Lab task 6/display.php
@@ -1,19 +1,24 @@
 <?php
-    session_start();
-    if(isset($_SESSION['msg'])){
-        echo $_SESSION['msg'];
-        unset($_SESSION['msg']);
+    $con = mysqli_connect('localhost', 'root','','product_db');
+    $sql = "select * from products";
+    $result = mysqli_query($con, $sql);
+    // $data = mysqli_fetch_assoc($result);
+    // print_r($data);
+
+    echo "<table border=1> 
+            <tr>
+                <th>NAME</th>
+                <th>PROFIT</th>
+                <th>EDIT & DELETE</th>
+            </tr>";
+
+    while($data  = mysqli_fetch_assoc($result)){
+        echo    "<tr>
+                    <td>{$data['Name']}</td>        
+                    <td>{$data['BuyingPrice']}</td>        
+                    <td>{$data['SellingPrice']}</td>        
+                </tr>";
     }
+
+    echo "</table>";
 ?>
-<html>
-<head>
-    <title>Products</title>
-</head>
-<body>
-    <table border='2'>
-        <tr><td><a href="task_A.php">Add Products</a></td></tr>
-        <tr><td><a href="task_B.php">Display</a></td></tr>
-        <tr><td><a href="search.php">Search</a></td></tr>
-    </table>
-</body>
-</html>
--- Final/Lab task 6/task_aVal.php
+++ Final/Lab task 6/task_aVal.php
@@ -3,28 +3,30 @@
     $name=$_POST['name'];
     $buingp=$_POST['buingp'];
     $sellingp=$_POST['sellingp'];
+    $display=$_POST['display'];
 
-    if(empty($name) || empty($buingp) || empty($sellingp)){
+    if(empty($name) || empty($buingp) || empty($sellingp))
+    {
         $_SESSION['err']="All must be filled up";
         header('location: task_A.php');
     }
-    else{
+    
+        
+    else if(empty($display))
+    {
+            header('location: display.php');
+    }
+    else if($display == 'Yes')
+    {
         $con = mysqli_connect('localhost', 'root', '', 'product_db');
-        if(empty($_POST['display'])){
-            $display="No";
-        }
-        else{
-            $display="Yes";
-        }
         $sql1 ="INSERT INTO `products`(`Name`, `BuyingPrice`, `SellingPrice`, `Display`) VALUES ('{$name}','{$buingp}','{$sellingp}','{$display}')";
         $result1 = mysqli_query($con, $sql1);
         if($result1>0){
             $_SESSION['msg']="Add successful<br>";
             header('location: display.php');
         }
-        else{
-            $_SESSION['msg']="Add is not successful<br>";
-            header('location: task_A.php');
-        }
+
     }
+        
+    
 ?>
